b1894f939cb23aa4e5176e0c09b96c1d6c3c44bf
Viktoria K. <122076719+toridotzip@users.noreply.github.com>
2026-04-20 20:34:36 +0200

    syncthing

---
+++ modules/syncthing.nix
@@ -0,0 +1,42 @@
+{ pkgs, config, ... }:
+
+{
+  age.secrets.syncthing-gui = {
+    file = ../secrets/syncthing-gui.age;
+  };
+
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = false;
+    guiAddress = "127.0.0.1:8384";
+    guiPasswordFile = config.age.secrets.syncthing-gui.path;
+    user = "etcvi";
+    configDir = "/home/etcvi/syncthing/config";
+    dataDir = "/home/etcvi/syncthing/data";
+    settings = {
+      gui.user = "etcvi";
+      devices = {
+        "Parsley" = { id = "EGA7AYA-PZC6GZC-H5GMLTN-LNLDX24-KYSIULI-B3MGHVT-H46Q4VC-7XWJEQU"; };
+      };
+      folders = {
+        "Notes" = {
+          path = "/home/etcvi/notes";
+          devices = [ "Parsley" ];
+        };
+      };
+    };  
+  };
+
+  networking.firewall = {
+    interfaces = {
+      "tailscale0" = {
+        allowedTCPPorts = [ 22000 ];
+        allowedUDPPorts = [ 21027 ];
+      };
+    };
+    extraCommands = ''
+      iptables -A nixos-fw -p tcp --dport 22000 -s 192.168.42.0/24 -j nixos-fw-accept
+      iptables -A nixos-fw -p udp --dport 21027 -s 192.168.42.0/24 -j nixos-fw-accept
+    '';
+  };
+}
